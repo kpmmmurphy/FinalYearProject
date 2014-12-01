518a99eaf5df2320f985563c2cd83f3c0fe4a562
kpmmmurphy <kpmmmurphy@gmail.com>
2014-12-01 23:13:52 +0000

    Added default configuration scripts for building wifi direct infrastructure

--- wifi_direct/setup_wifi_direct.sh
+++ wifi_direct/setup_wifi_direct.sh
@@ -1,10 +1,10 @@
-#Sets up Wifi Direct
+#Sets up all neeeded componented for Wifi Direct on the Raspberry Pi
 
 #Author: Kevin Murphy
 #Date  : 27 - Oct - 14
 
 #Get the latest version of the wpa_supplicant and hostapd
-git clone git://w1.fi/srv/git/hostap.git
+#git clone git://w1.fi/srv/git/hostap.git
 
 #Install Hostapd
 #sudo apt-get install hostapd
@@ -19,8 +19,9 @@ git clone git://w1.fi/srv/git/hostap.git
 #Install the dev version of OpenSSL
 sudo apt-get install libssl-dev
 
-#Install Netlink Protocol Library Suite
+#Install Netlink Protocol Library Suite	
 sudo apt-get install libnl-3-dev
+sudo apt-get install libnl-genl-3-dev
 
 #Install libpcsclite for interfacing with smart cards 
 sudo apt-get install libpcsclite-dev 
@@ -34,8 +35,11 @@ sudo apt-get install libreadline-dev
 #Install LibnCurses
 sudo apt-get install libncurses5-dev
 
-#Copy custom wpa_supplicant .config file 
-#---------
+#Untar the files
+tar -zxvf ./wpa_supplicant-2.3.tar.gz
+
+#Copy custom wpa_supplicant .config file
+sudo cp ./config/def_p2p_config ./wpa_supplicant-2.3/wpa_supplicant/.config
 
 #Make and make install wpa_supplicant
 sudo ./wpa_supplicant-2.3/wpa_supplicant/ make BINDIR=/sbin LIBDIR=/lib && sudo ./wpa_supplicant-2.3/wpa_supplicant/ make install
