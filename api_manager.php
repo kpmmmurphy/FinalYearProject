0a7751ce58310e67ee48dc9d9b88af3e6ec3aaec
kpmmmurphy <kpmmmurphy@gmail.com>
2014-12-23 15:08:43 +0000

    PHP API Class now inserting sensor values into DB correctly

--- api_manager.php
+++ api_manager.php
@@ -3,36 +3,33 @@
     //Author :: Kevin Murphy
     //Date   :: 16 - Dec - 14
 
-    //Costants
+    //---Constants
     $debug = true;
+    //Request Params
     define('PARAM_SERVICE', 'service');
     define('PARAM_GET_CONFIG', 'get_config');
     define('PARAM_UPDATE_CONFIG', 'update_config');
     define('PARAM_UPLOAD_SENSOR_VALUES', 'upload_sensor_values');
     define('PARAM_SENSOR_VALUES', 'sensor_values');
+    //SQL Tables
+    define('SQL_TABLE_CURRENT', 'current_day_sensor_output');
 
-    $serverName = "localhost";
-    $username   = "kpm2";
-    $password   = "shiegeib";
-    $database   = "2015_kpm2";
+    $headers    = getallheaders();
+    $rawRequest = file_get_contents('php://input', 0, null, null);
+    $requestObj = json_decode($rawRequest);
 
-    $request = file_get_contents('php://input', 0, null, null);
-    var_dump(json_decode($request));
-    #echo $request->service;
     if($debug){
-        echo "\n";
+        echo "\n\nHEADERS::\n\n";
         foreach (getallheaders() as $name => $value) {
             echo "$name: $value\n";
         }
+
+        echo "\n\nREQUEST::\n\n";
+        var_dump($requestObj);
+
+        echo "\n";
     }
-    
-    #$jsonString = file_get_contents('php://input', 0, null, null);
-    #$request = json_decode($jsonString , true);
-    #echo "\n" . "Motion" . $request['motion'] . "\n";
-    #foreach ($request as $name => $value) {
-    #        echo "$name: $value\n";
-    #    }
-    $headers = getallheaders();
+
     
     if(isset($headers[constant('PARAM_SERVICE')])){
         $requestedService = htmlspecialchars($headers[constant('PARAM_SERVICE')]);
@@ -52,7 +49,7 @@
             case constant('PARAM_UPLOAD_SENSOR_VALUES'):
                 //Insert Sensor value 
                 echo "\nInsert Sensor Values into DB\n";
-                #insertSensorValues($sensorValues);
+                insertSensorValues($requestObj);
                 break;
 
         }
@@ -61,10 +58,34 @@
     }
 
 function insertSensorValues($values)
-{
-    $json_values = json_encode($values);
-    
-    echo "Motion" . $json_values->motion;
-}
+{   
+    $serverName = "localhost";
+    $username   = "kpm2";
+    $password   = "shiegeib";
+    $database   = "2015_kpm2";
+    $conn = new mysqli($serverName, $username, $password, $database);
 
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $date_and_time   = date("l jS \of F Y h:i:s A");
+    $motion          = $values->motion;
+    $temperature     = $values->temperature;
+    $carbon_monoxide = $values->carbon_monoxide;
+    $flammable_gas   = $values->flammable_gas;
+
+    $sql = "INSERT INTO ". constant('SQL_TABLE_CURRENT') . 
+           " (carbon_monoxide, temperature, flammable_gas, motion)
+             VALUES ('$carbon_monoxide', '$temperature', '$flammable_gas', '$motion')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
 ?>
