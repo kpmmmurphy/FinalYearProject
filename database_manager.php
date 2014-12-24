72a57e606d81e974205fdd7a3b0de74bdb225b6f
kpmmmurphy <kpmmmurphy@gmail.com>
2014-12-24 15:40:36 +0000

    PHP Database Manager now uses PDO instead of mysqli

--- database_manager.php
+++ database_manager.php
@@ -7,25 +7,47 @@ class DatabaseManager
 
     private $conn = NULL;
 
-    private $serverName = "localhost";
+    private $servername = "localhost";
     private $username   = "kpm2";
     private $password   = "shiegeib";
     private $database   = "2015_kpm2";
 
 	function __construct()
 	{
-        $this->conn = new mysqli($this->serverName, $this->username, $this->password, $this->database);
+        #$this->conn = new mysqli($this->serverName, $this->username, $this->password, $this->database);
+        $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // Check connection
-	    if ($this->conn->connect_error) {
-	        die("Connection failed: " . $conn->connect_error);
-	    }
+	    //if ($this->conn->connect_error) {
+	    //    die("Connection failed: " . $conn->connect_error);
+	    //}
 	}
 
 	function __destruct() {
-        self::closeConn();
+        $this->closeConn();
     }
 
     public function insertSensorValues($values)
+    {
+    	try
+    	{
+            $sql = $this->conn->prepare("INSERT INTO ". self::SQL_TABLE_CURRENT . 
+	           " (carbon_monoxide, temperature, flammable_gas, motion)
+	             VALUES (:carbon_monoxide, :temperature, :flammable_gas, :motion)");
+        
+            $sql->bindParam(":carbon_monoxide", $values->carbon_monoxide);
+            $sql->bindParam(":temperature"    , $values->temperature);
+            $sql->bindParam(":flammable_gas"  , $values->flammable_gas);
+            $sql->bindParam(":motion"         , $values->motion);
+            $sql->execute();
+    	}
+    	catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+    	}  
+	}
+
+	public function selectLatestSensorValues()
     {
 	    $date_and_time   = date("l jS \of F Y h:i:s A");
 	    $motion          = $values->motion;
@@ -33,9 +55,9 @@ class DatabaseManager
 	    $carbon_monoxide = $values->carbon_monoxide;
 	    $flammable_gas   = $values->flammable_gas;
 
-	    $sql = "INSERT INTO ". self::SQL_TABLE_CURRENT . 
-	           " (carbon_monoxide, temperature, flammable_gas, motion)
-	             VALUES ('$carbon_monoxide', '$temperature', '$flammable_gas', '$motion')";
+	    $sql = "SELECT * ". 
+	           "FROM " . self::SQL_TABLE_CURRENT . 
+	           "WHERE id = MAX(id)";
 
 	    if ($this->conn->query($sql) === TRUE) {
 	        echo "New record created successfully";
@@ -46,7 +68,8 @@ class DatabaseManager
 
 	private function closeConn()
 	{
-		$this->conn->close();
+		#$this->conn->close();
+		$this->conn = null;
 	}
 
 }
