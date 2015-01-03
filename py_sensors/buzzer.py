a9f7bc48f9ad1774184ef0f50d5d7bacdef30a0c
kpmmmurphy <kpmmmurphy@gmail.com>
2015-01-03 18:48:48 +0000

    Fixed sleeping bug in Buzzer class

--- py_sensors/buzzer.py
+++ py_sensors/buzzer.py
@@ -11,15 +11,16 @@ class Buzzer(object):
 
 	@staticmethod
 	def buzz():
-		sleep_time = 200
+		sleep_time = 0.2
 		#set up GPIO using BCM numbering
 		GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18, GPIO.OUT)
-
-        GPIO.output(18, True)
-        time.sleep(sleep_time)
-        GPIO.output(18, False)
-        time.sleep(sleep_time)
-        GPIO.output(18, True)
-        time.sleep(sleep_time)
-        GPIO.output(18, False)
+		GPIO.setup(18, GPIO.OUT)
+		
+		GPIO.output(18, True)
+		time.sleep(sleep_time)
+		GPIO.output(18, False)
+		time.sleep(sleep_time)
+		GPIO.output(18, True)
+		time.sleep(sleep_time)
+		GPIO.output(18, False)
+		GPIO.cleanup()
