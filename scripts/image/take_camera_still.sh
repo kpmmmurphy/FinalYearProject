a31a46b70431ed51f415bd23bceb017e1c21d78b
Kevin Murphy <kpmmmurphy@gmail.com>
2015-01-18 16:19:27 +0000

    Images working corectly again

--- scripts/image/take_camera_still.sh
+++ scripts/image/take_camera_still.sh
@@ -11,9 +11,9 @@ if [ ! -d "/home/pi/FinalYearProject/camera/still_backup" ]; then
   mkdir -p /home/pi/FinalYearProject/camera/still_backup
 fi
 
-mv ~/FinalYearProject/camera/still/* ~/FinalYearProject/camera/still_backup/
+mv /home/pi/FinalYearProject/camera/still/* /home/pi/FinalYearProject/camera/still_backup/
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 #DATE=$(zdump GMT)
 
-raspistill -vf -hf --width 1080 --height 720 --quality 100 -o ~/FinalYearProject/camera/still/$DATE.jpg
+raspistill -vf -hf --width 1080 --height 720 --quality 100 -o /home/pi/FinalYearProject/camera/still/$DATE.jpg
