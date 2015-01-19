8123dabbdc51de46273ccc0d4d68a0360b5df72e
kpmmmurphy <kpmmmurphy@gmail.com>
2015-01-19 22:47:10 +0000

    Working on video uploading

--- php/api_manager.php
+++ php/api_manager.php
@@ -6,7 +6,7 @@
     require_once('database_manager.php');
 
     //---Constants
-    $debug = false;
+    $debug = true;
     //Request Params
     define('PARAM_SERVICE', 'service');
     define('PARAM_GET_CONFIG', 'get_config');
@@ -135,7 +135,7 @@
 
 function verifiy_and_upload_file($fileName)
 {
-    $debug = false;
+    $debug = true;
     $target_file = constant("DIR_CAMERA") . basename($_FILES[$fileName]["name"]);
     $check = getimagesize($_FILES[$fileName]["tmp_name"]);
     $uploadOk = 1;
@@ -154,7 +154,7 @@ function verifiy_and_upload_file($fileName)
     }
 
     // Check file size
-    if ($_FILES[$fileName]["size"] > 500000) {
+    if ($_FILES[$fileName]["size"] > 1000000) {
         if($debug){
             echo "Sorry, your file is too large.";
         }
